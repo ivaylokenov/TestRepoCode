9d4d704bdf19bb9aeea6189a0ead9dda5b927944
Ivaylo Kenov <ivaylo.kenov@gmail.com>
2019-07-15 21:00:37 +0300

    Initial code added

---
+++ TestGHApp/TestGHApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TestGHApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var a = 5;
+
+            Console.WriteLine(a);
+        }
+    }
+}
